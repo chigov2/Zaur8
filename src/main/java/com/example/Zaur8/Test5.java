a0c491a195be72e3d4dd7d775ee4e5a3386ec1a2
Max2 <chigov@gmail.com>
2022-07-02 08:58:42 +0300

    test5

--- src/main/java/com/example/Zaur8/Test5.java
+++ src/main/java/com/example/Zaur8/Test5.java
@@ -5,10 +5,8 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import java.util.List;
 
-
-public class Test4 {
+public class Test5 {
     public static void main(String[] args) {
         SessionFactory factory = new Configuration()
                 .configure("hibernate.cfg.xml")
@@ -17,9 +15,7 @@ public class Test4 {
         try {
             Session session = factory.getCurrentSession();
             session.beginTransaction();
-//            Employee employee3 = session.get(Employee.class,1);
-//            employee3.setSalary(2500);
-            session.createQuery("update Employee set salary=1000 where name='Mike'").executeUpdate();
+
 
             session.getTransaction().commit();
             System.out.println("Done");
